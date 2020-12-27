2ec4b0836a02923bee1091409d708e28ebe1bc1f
truetime0310 <739677817@qq.com>
2020-12-27 20:36:20 +0800

    improvement1

--- app/src/main/java/com/example/myandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/myandroidproject/MainActivity.kt
@@ -8,9 +8,14 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(),View.OnClickListener {
     private val currentInputNumSB=StringBuilder()
-    private val numsList= mutableListOf<Int>()
+    private val numsList= mutableListOf<String>()
     private val operatorsList= mutableListOf<String>()
-//    数字开始的标识符
+    //判断是否为乘除后的正负
+    private var ope=""
+    private var plusOrMinus=true
+    //判断输入的第一个字符是数字还是运算符号
+    private var flagzero=true
+    //数字开始的标识符
     private var isNumStart=true
 
     @ExperimentalStdlibApi
@@ -49,7 +54,11 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         num7.setOnClickListener(this)
         num8.setOnClickListener(this)
         num9.setOnClickListener(this)
-
+        decimal.setOnClickListener(this)
+        //等于符号
+        equalSymbol.setOnClickListener {
+            equalButtonClicked(it)
+        }
     }
 
     override fun onClick(v: View?) {
@@ -61,34 +70,79 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
     fun numberButtonClick(view: View){
         //将view强制转换为TextView
         val tv=view as TextView
-
-        currentInputNumSB.append(tv.text)
-        if(isNumStart){
-            //当前输入的是一个新的数字，添加到数组中
-            numsList.add(tv.text.toString().toInt())
-            //更改isNumStart状态
-            isNumStart=false
-        }else{
+        //对小数点的限制
+        var flag=true
+        if(tv.text.toString()=="." &&
+            (numsList.size==0 || numsList.size==operatorsList.size)){
+                flag=false
+        }
+        if(flag){
+            currentInputNumSB.append(tv.text)
+            if(isNumStart){
+                //判断是否为正负--即numsList中最后一个存的+或-
+                if(plusOrMinus==false){
+                    var num=tv.text.toString().toDouble();
+                    //算出这个位置应该存的值
+                    var result=realCalculate(0.0,ope,num)
+                    numsList.add(result.toString())
+                    plusOrMinus==true
+                    isNumStart=false
+                } else{
+                    //当前输入的是一个新的数字，添加到数组中
+                    numsList.add(tv.text.toString())
+                    //更改isNumStart状态
+                    isNumStart=false
+                }
+            }else{
             //用当前的数字去替换数组中最后一个元素
-            numsList[numsList.size-1]=currentInputNumSB.toString().toInt()
+            numsList[numsList.size-1]=currentInputNumSB.toString()
         }
         //显示内容
         showUI()
         //计算结果
         calculate()
+        }
     }
 
     //运算符键
     fun operatorButtonClicked(view: View){
+        plusOrMinus=true
         //将view强制转换为TextView
         val tv=view as TextView
-        //保存当前运算符
-        operatorsList.add(tv.text.toString())
-        //改变状态
-        isNumStart=true
-        currentInputNumSB.clear()
-        //显示内容
-        showUI()
+        //输入第一个即为数字
+        if(numsList.size>0){
+            //一个数字一个运算符
+            if(operatorsList.size<numsList.size){
+                //保存当前运算符
+                operatorsList.add(tv.text.toString())
+                //改变状态
+                isNumStart=true
+                currentInputNumSB.clear()
+                //显示内容
+                showUI()
+            }else{
+                //运算符较多，即*-类似
+                val si=operatorsList.size-1
+                if((operatorsList[si]=="÷"||operatorsList[si]=="x")&&(tv.text.toString()=="+"||tv.text.toString()=="—")){
+                    ope=tv.text.toString()
+                    plusOrMinus=false
+                }
+            }
+        }else{
+          if(tv.text.toString()=="—"||tv.text.toString()=="+"){
+              //输入的第一个为运算符，自动添加numsList第一个值为0
+              numsList.add("0")
+              //flagzero为下方显示所用
+              flagzero=false
+              //保存当前运算符
+              operatorsList.add(tv.text.toString())
+              //改变状态
+              isNumStart=true
+              currentInputNumSB.clear()
+              //显示内容
+              showUI()
+          }
+        }
     }
 
     //清空键--输入框和保存的StringBuilder
@@ -99,6 +153,9 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         operatorsList.clear()
         numsList.clear()
         isNumStart=true
+        plusOrMinus=true
+        flagzero=true
+
     }
 
     //撤销键
@@ -129,7 +186,14 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
 
     //等于键
     fun equalButtonClicked(view: View){
-
+        var str=result_textview.text.toString()
+        process_textview.text=str
+        result_textview.text=""
+        currentInputNumSB.clear()
+        operatorsList.clear()
+        numsList.clear()
+        numsList.add(str)
+        isNumStart=true
     }
 
     //拼接当前运算的表达式，显示到界面上
@@ -137,8 +201,16 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
         //页面显示内容--拼接后的总内容
         val str=StringBuilder()
         for((i,num) in numsList.withIndex()){
+            //判断是否为0
+            if(numsList[0]=="0" && i==0){
+                flagzero=false
+            }else{
+                flagzero=true
+            }
             //将当前的数字拼接
-            str.append(num)
+            if(flagzero==true){
+                str.append(num)
+            }
             //判断运算符数组中对应位置是否有内容
             if(operatorsList.size>i){
                 //将i对应的运算符拼接到字符串中
@@ -156,8 +228,8 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
             //i记录运算符数组遍历时的下标
             var i=0
             //记录第一个运算符--数字数组的第一个数
-            var param1=numsList[0].toFloat()
-            var param2=0.0f
+            var param1=numsList[0].toDouble()
+            var param2=0.0
             if(operatorsList.size>0){
                 while (true){
                     //获取i对应的运算符
@@ -166,7 +238,7 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
                     if(operator=="x"||operator=="÷"){
                         //乘除直接运算--找到第二个运算数
                         if(i+1<numsList.size){
-                            param2=numsList[i+1].toFloat()
+                            param2=numsList[i+1].toDouble()
                             //运算
                             param1=realCalculate(param1,operator,param2)
                         }
@@ -175,19 +247,19 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
                         if(i==operatorsList.size-1 ||
                             (operatorsList[i+1]!="x" && operatorsList[i+1]!="÷")){
                             if(i<numsList.size-1){
-                                param2=numsList[i+1].toFloat()
+                                param2=numsList[i+1].toDouble()
                                 param1=realCalculate(param1,operator,param2)
                             }
                         }else{
                             //后面有而且是乘除
                             var j=i+1
-                            var mparm1=numsList[j].toFloat()
-                            var mparm2=0.0f
+                            var mparm1=numsList[j].toDouble()
+                            var mparm2=0.0
                             while(true){
                                 //获取j对应的运算符
                                 if(operatorsList[j]=="x"||operatorsList[j]=="÷"){
-                                    if(j<operatorsList.size-1){
-                                        mparm2=numsList[j+1].toFloat()
+                                    if(j<numsList.size-1){
+                                        mparm2=numsList[j+1].toDouble()
                                         mparm1=realCalculate(mparm1,operatorsList[j],mparm2)
                                     }
                                 }else{
@@ -214,15 +286,17 @@ class MainActivity : AppCompatActivity(),View.OnClickListener {
                 }
             }
             //显示对应的结果
-            result_textview.text= String.format("%.1f",param1)
+            //result_textview.text= String.format("%.4f",param1)
+            result_textview.text=param1.toFloat().toString()
         }else{
             result_textview.text="0"
         }
     }
 
+
     //运算
-    private fun realCalculate(param1:Float,operator: String,param2:Float):Float{
-        var result:Float=0.0f
+    private fun realCalculate(param1:Double,operator: String,param2:Double):Double{
+        var result:Double=0.0
         when(operator){
             "+"->{
                 result=param1+param2
