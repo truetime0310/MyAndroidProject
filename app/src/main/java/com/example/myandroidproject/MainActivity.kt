901562f63372cc25c9d53d2826b402752b689b40
truetime0310 <739677817@qq.com>
2020-12-27 13:33:23 +0800

    初始化

--- app/src/main/java/com/example/myandroidproject/MainActivity.kt
+++ app/src/main/java/com/example/myandroidproject/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.myandroidproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    //数字键
+    fun numberButtonClick(view: View){
+
+    }
+
+    //运算符键
+    fun operatorButtonClicked(view: View){
+
+    }
+
+    //清空键
+    fun clearButtonClicked(view: View){
+
+    }
+
+    //撤销键
+    fun backButtonClicked(view: View){
+
+    }
+
+    //等于键
+    fun equalButtonClicked(view: View){
+
     }
 }
